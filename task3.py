4de15d4b80b72ffa5ffcff3f195d54d128e3e40e
Aryan Tewatia <aryantewatia2006@gmail.com>
2026-09-19 23:52:31 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,8 @@
+num1 = float(input("enter num1: "))
+num2 = float(input("enter num2: "))
+
+print("addition:", num1+num2)
+print("subtraction:", num1-num2)
+print("multiplication:", num1*num2)
+print("division:", num1/num2)
+print("modulus:", num1%num2)
