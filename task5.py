e9a28a581a99aa96cd3357611ccfde79f2647c34
Aryan Tewatia <aryantewatia2006@gmail.com>
2026-09-19 23:57:37 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,11 @@
+print("numbers from 1 to 20")
+for i in range(1,21):
+    print(i)
+num=int(input("enter num: "))
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
+print("even numbers from 1 to 50")
+i = 2
+while i <= 50:
+    print(i)
+    i=i+2
